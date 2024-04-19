0d0fad317f70ef8f90ee3dd9b35bba6ce676cea0
MohammedHElmasry <01122437793rr@gmail.com>
2024-04-19 16:07:40 +0200

    Ref

--- db/connection.js
+++ db/connection.js
@@ -3,12 +3,13 @@ dotenv.config()
 import mongoose from "mongoose";
 
 export const connect = async ()=>{
-   await mongoose.connect(`mongodb+srv://01140775155dd:KDSTycDdfVkEbAKJ@cluster0.nypuppi.mongodb.net/`).then(res=>{
+   await mongoose.connect(`mongodb+srv://01140775155dd:*khW-qEEPp6E9-E@cluster0.nypuppi.mongodb.net/`).then(res=>{
     console.log(`connect db`);
    }).catch(err=>{
     console.log(err);
    });
 }
+// *khW-qEEPp6E9-E pass
 
 
 export default connect
