970f55e1ee23659dd19e8d757bb5517db088da75
MohammedHElmasry <01122437793rr@gmail.com>
2024-04-19 19:24:43 +0200

    refre

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import express from 'express'
 import connect from './db/connection.js'
 import bootstrap from './index.router.js'
 const app = express()
-const port=5000
+const port= process.env.PORT || 5000
 
 connect()
 bootstrap(app,express)
 app.listen(port,()=>{
     console.log(`app run in port num ${port}`);
+    console.log(port);
 })
