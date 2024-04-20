6c9a90b1d62ef4ed01b0a3e7931baf8dadbee6ee
MohammedHElmasry <01122437793rr@gmail.com>
2024-04-20 06:08:04 +0200

    ref

--- src/modules/trips/trip.router.js
+++ src/modules/trips/trip.router.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import * as tripcontrooler from './controller/trip.js'
 const router = Router()
 
-router.get('/',tripcontrooler.hotels)
+router.get('/',tripcontrooler.trips)
 router.post('/create',tripcontrooler.createtrip)
 router.put('/update/:id',tripcontrooler.updatetrip)
 router.delete('/delete/:id',tripcontrooler.deletedtrip)
