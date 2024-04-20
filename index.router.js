8fd1effdbeb4329ea4e51967d81e67feacfa0e7a
MohammedHElmasry <01122437793rr@gmail.com>
2024-04-20 23:45:26 +0200

    ref

--- index.router.js
+++ index.router.js
@@ -4,24 +4,26 @@ import { asyncHandler, handelerror } from "./src/utils/errorhandling.js"
 import city from './src/modules/city/city.router.js'
 import hotel from './src/modules/hotel/hotel.router.js'
 import trip from './src/modules/trips/trip.router.js'
-import cors from 'cors' 
+import cors from 'cors'
 
 
 
-const bootstrap= (app,express)=>{
+const bootstrap = (app, express) => {
     app.use(express.json())
-    app.use('/user',user)
-    app.use('/auth',auth)
-    app.use('/city',city)
-    app.use('/hotel',hotel)
-    app.use('/trip',trip)
+    app.use('/user', user)
+    app.use('/auth', auth)
+    app.use('/city', city)
+    app.use('/hotel', hotel)
+    app.use('/trip', trip)
     app.use(handelerror)
     app.use(asyncHandler)
     app.use(cors({
-        origin: 'http://127.0.0.1:5500', 
-      }));
+        origin: 'http://127.0.0.1:5500',
+    }));
+    app.use('*', (req, res, next) => {
+        return res.status(404).json({ message: 'Invalid routing' });
+    });
 
-    
 }
 
 
